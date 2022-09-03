c1baeb2b50fed9156253028d357841f54029f081
Abdullah <waygrozny95@gmail.com>
2022-09-03 14:12:04 +0300

    Created service & repository interfaces.

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"github.com/itisnotyourenv/todo-go"
 	"github.com/itisnotyourenv/todo-go/pkg/handler"
+	"github.com/itisnotyourenv/todo-go/pkg/repository"
+	"github.com/itisnotyourenv/todo-go/pkg/service"
 	"log"
 )
 
 const Port = "8080"
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	log.Println("Starting server on port", Port)
-	handlers := new(handler.Handler)
 	srv := new(todo.Server)
 	if err := srv.Run(Port, handlers.InitRoutes()); err != nil {
 		log.Fatalln(err)
---
+++ pkg/repository/repository.go
@@ -0,0 +1,21 @@
+// логика общения с БД
+package repository
+
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository() *Repository {
+	return &Repository{}
+}
